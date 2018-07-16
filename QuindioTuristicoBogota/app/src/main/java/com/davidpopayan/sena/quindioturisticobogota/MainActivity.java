4f9edd44ef2c662a8a82da77446074b0a633078c
DavidPyn27 <popayan159@gmail.com>
2018-07-16 17:09:53 -0500

    commita1

--- QuindioTuristicoBogota/app/src/main/java/com/davidpopayan/sena/quindioturisticobogota/MainActivity.java
+++ QuindioTuristicoBogota/app/src/main/java/com/davidpopayan/sena/quindioturisticobogota/MainActivity.java
@@ -1,7 +1,11 @@
 package com.davidpopayan.sena.quindioturisticobogota;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, prueba.class);
+                startActivity(intent);
+                finish();
+            }
+
+        };
+        Timer timer = new Timer();
+        timer.schedule(timerTask, 3000);
     }
 }
